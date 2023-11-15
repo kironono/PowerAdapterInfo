a6088d72babd0c77bb2d820b5b1f65b86883c5ff
Junichi Kakisako <kilo.sw@gmail.com>
2023-11-15 21:22:13 +0900

    Initial Commit

---
+++ PowerAdapterInfo/PowerAdapterInfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PowerAdapterInfoApp.swift
+//  PowerAdapterInfo
+//
+//  Created by Junichi Kakisako on 2023/11/15.
+//
+
+import SwiftUI
+
+@main
+struct PowerAdapterInfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
